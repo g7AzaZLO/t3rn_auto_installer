afe330101fd7be05b96002ad90f98edcf2c303a1
ZLO#DEV <59707245+g7AzaZLO@users.noreply.github.com>
2024-10-15 22:22:59 +0300

    Update and rename t3rn_installer.sh to t3rn_auto_installer.sh

--- t3rn_auto_installer.sh
+++ t3rn_auto_installer.sh
@@ -1,30 +1,22 @@
 #!/bin/bash
-
 echo "Загрузка исполняемого файла..."
 wget https://github.com/t3rn/executor-release/releases/download/v0.21.7/executor-linux-v0.21.7.tar.gz -O executor-linux.tar.gz
-
 echo "Распаковка архива..."
 tar -xzvf executor-linux.tar.gz
 cd executor || exit
-
 echo "Настройка параметров среды..."
 export NODE_ENV="testnet"
 export LOG_LEVEL="debug"
 export LOG_PRETTY="false"
 export EXECUTOR_PROCESS_ORDERS="true"
 export EXECUTOR_PROCESS_CLAIMS="true"
-
 echo "Введите ваш приватный ключ:"
 read -r PRIVATE_KEY_LOCAL
 export PRIVATE_KEY_LOCAL="$PRIVATE_KEY_LOCAL"
-
 export ENABLED_NETWORKS="arbitrum-sepolia,base-sepolia,optimism-sepolia,l1rn"
-
 echo "Запуск узла в screen сессии..."
 cd /root/executor/executor/bin/ || exit
-
 screen -dmS executor_session bash -c './executor; exec bash'
-
 echo "Установка и запуск завершены. Узел запущен в screen-сессии с именем 'executor_session'."
 echo -e "\033[1;36m"
 echo -e "████╗░██████╗░███████╗████╗  ░█████╗░███████╗░█████╗░███████╗██╗░░░░░░█████╗░"
